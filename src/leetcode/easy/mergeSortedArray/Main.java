45760b3f18c85fe3759868cafb76c4de818becab
phoenix <phoenixzero4@gmail.com>
2025-03-16 11:25:01 -0400

    MergeSortedArraySolution

---
+++ src/leetcode/easy/mergeSortedArray/Main.java
@@ -0,0 +1,14 @@
+package leetcode.easy.mergeSortedArray;
+
+public class Main {
+
+    
+    public static void main(String...strings ){
+	int[] a = {1,2,3,0,0,0};
+	int m = 3;
+	int[] b = {2,5,6};
+	int n = 3;
+	
+	Solution.merge(a,m,b,n);
+    }
+}
